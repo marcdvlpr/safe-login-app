1d6c8272ff97488fcc6aa693870aa8ddeffb56da
Marc <marcdvlpr@gmail.com>
2024-06-03 22:18:59 +0200

    Init project

---
+++ SafeLogin/SafeLogin/SafeLoginApp.swift
@@ -0,0 +1,32 @@
+//
+//  SafeLoginApp.swift
+//  SafeLogin
+//
+//  Created by Apprenant 124 on 03/06/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SafeLoginApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
