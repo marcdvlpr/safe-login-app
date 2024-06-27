88cbab7a102611fb383017491fee1af211f16837
Marc <marcdvlpr@gmail.com>
2024-06-27 14:41:37 +0200

    Add fetchUser method to AuthViewModel

--- SafeLogin/SafeLogin/ViewModel/AuthViewModel.swift
+++ SafeLogin/SafeLogin/ViewModel/AuthViewModel.swift
@@ -9,13 +9,20 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
+@MainActor
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
 
     let dbFire = Firestore.firestore()
 
-    init() {}
+    init() {
+        self.userSession = Auth.auth().currentUser
+        
+        Task {
+            await fetchUser()
+        }
+    }
 
     func createUser(email: String, password: String) async throws {
         do {
@@ -27,5 +34,12 @@ class AuthViewModel: ObservableObject {
         } catch {
             print("DEBUG: Failed to create user with error \(error.localizedDescription)")
         }
+    }
+
+    func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try? await dbFire.collection("users").document(uid).getDocument()
+        guard let user = try? snapshot?.data(as: User.self) else { return }
+        self.currentUser = user
     }
 }
